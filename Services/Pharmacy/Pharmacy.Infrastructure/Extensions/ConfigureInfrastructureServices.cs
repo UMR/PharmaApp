ae1a1c02eb8f84ce1cb089adb8854fe39d6120f6
ashorif <ashorif@ael-bd.com>
2025-02-26 16:40:05 +0600

    Added project reference and try implementing web builder extension method.

---
+++ Services/Pharmacy/Pharmacy.Infrastructure/Extensions/ConfigureInfrastructureServices.cs
@@ -0,0 +1,12 @@
+﻿//using Microsoft.AspNetCore.Builder;
+
+namespace Pharmacy.Infrastructure.Extensions
+{
+    //public static class ConfigureInfrastructureServices
+    //{
+    //    public static WebApplicationBuilder AddInfrastructureServices(this WebApplicationBuilder builder)
+    //    {
+    //        return builder;
+    //    }
+    //}
+}
