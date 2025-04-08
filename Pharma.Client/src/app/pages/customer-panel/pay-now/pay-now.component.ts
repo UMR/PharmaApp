c1c37032ec19e3d8e85a0f28ee8837fd2fc3b7bf
Fazlay Rabby Shanto <fshanto@ael-bd.com>
2025-04-08 12:44:36 +0600

    changes

--- Pharma.Client/src/app/pages/customer-panel/pay-now/pay-now.component.ts
+++ Pharma.Client/src/app/pages/customer-panel/pay-now/pay-now.component.ts
@@ -112,9 +112,11 @@ export class PayNowComponent implements OnInit {
           next: (res) => {
             this.toastService.showSuccess("success", 'Payment successful');
             this.displayModal = false;
+            window.location.href = '/';
           },
           error: (err) => {
             this.toastService.showError("failed", 'Payment failed');
+            window.location.href = '/';
           }
         });
 
